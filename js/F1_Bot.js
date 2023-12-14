a753f3e5e46308918d4d662c9d3d0ce97423c912
Gabriel Chiacchio <97293408+ga-chiacchio@users.noreply.github.com>
2023-12-13 22:34:32 -0300

    Update F1_Bot.js

--- js/F1_Bot.js
+++ js/F1_Bot.js
@@ -372,14 +372,15 @@ room.onGameTick = function(){
     // checkPlayerDRS();
     endRaceSession();
     currentTime += 1/60;
+    let randomDistance = (Math.round(Math.random())*2-1)*(Math.floor(Math.random() * (100 - 50 + 1))+50);
     let players = room.getPlayerList().filter(p => room.getPlayerDiscProperties(p.id) != null && playerList[p.name].isInTheTrack == true);
     if (currentTime >= 60) {
 	if(Math.floor(currentTime) % 15 == 0) {
 	camId = Math.ceil(Math.random() * players.length);
 	}
 	if(camId != null && room.getPlayerDiscProperties(camId).x != null && room.getPlayerDiscProperties(camId).y != null) {
-            room.setDiscProperties(0,{x: room.getPlayerDiscProperties(camId).x+1});
-            room.setDiscProperties(0,{y: room.getPlayerDiscProperties(camId).y+1});
+            room.setDiscProperties(0,{x: room.getPlayerDiscProperties(camId).x+randomDistance});
+            room.setDiscProperties(0,{y: room.getPlayerDiscProperties(camId).y+randomDistance});
 	    room.setDiscProperties(0,{xspeed: 0});
             room.setDiscProperties(0,{yspeed: 0})
     	}
