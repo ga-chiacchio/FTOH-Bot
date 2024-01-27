7c6d4d8ca6bc87c8dd093e9cd9ba98f54e7395eb
Gabriel Chiacchio <97293408+ga-chiacchio@users.noreply.github.com>
2024-01-27 17:42:15 -0300

    Update F1_Bot.js

--- js/F1_Bot.js
+++ js/F1_Bot.js
@@ -102,6 +102,9 @@ const teams = ["spectators","red","blue"];
 var generalSafetyCar = false;
 var currentTime = 0;
 var camId = null;
+var currentCircuit = 1;
+var JMap = JSON.parse(Circuits[id-1]);
+
 
 var isRoomSet = false;
 
@@ -313,6 +316,62 @@ function serialize(number){
     return number.toFixed(3);
 }
 
+function runCamera(){
+    let players = room.getPlayerList().filter(p => room.getPlayerDiscProperties(p.id) != null && playerList[p.name].isInTheTrack == true);
+    if (currentTime >= 28) {
+	if(Math.floor(currentTime) % 5 == 0) {
+	camId = Math.ceil(Math.random() * players.length);
+	// players.forEach(p => {
+	// 	if(playerList[p.name].id==camId){
+	// 		validCam = true;
+	// 	};
+	//  });
+	}
+	if(camId != null && room.getPlayerDiscProperties(camId) != null && room.getPlayerDiscProperties(camId).x != null) {
+            // room.setDiscProperties(0,{x: room.getPlayerDiscProperties(camId).x+randomDistance});
+            // room.setDiscProperties(0,{y: room.getPlayerDiscProperties(camId).y+randomDistance});
+            room.setDiscProperties(0,{x: room.getPlayerDiscProperties(camId).x});
+            room.setDiscProperties(0,{y: room.getPlayerDiscProperties(camId).y});
+	    room.setDiscProperties(0,{xspeed: 0});
+            room.setDiscProperties(0,{yspeed: 0})
+    	}
+    	};
+}
+
+function collisionDetectionSegmentPlayer() { //This function must be handled in room.onGameTick() to get the true result.
+    var players = room.getPlayerList();
+    for (var i = 0; i < players.length; i++) {
+        for (var j = 0; j < JMap.segments.length; j++) {
+            if (players[i].team == 1) {
+                var distancetov0 = pointDistance(room.getPlayerDiscProperties(players[i].id), JMap.vertexes[JMap.segments[j].v0]);
+                var distancetov1 = pointDistance(room.getPlayerDiscProperties(players[i].id), JMap.vertexes[JMap.segments[j].v1]);
+                var length = pointDistance(JMap.vertexes[JMap.segments[j].v0], JMap.vertexes[JMap.segments[j].v1]);
+                var dot = (((players[i].position.x - JMap.vertexes[JMap.segments[j].v0].x) * (JMap.vertexes[JMap.segments[j].v1].x - JMap.vertexes[JMap.segments[j].v0].x)) + ((players[i].position.y - JMap.vertexes[JMap.segments[j].v0].y) * (JMap.vertexes[JMap.segments[j].v1].y - JMap.vertexes[JMap.segments[j].v0].y))) / Math.pow(length, 2);
+                var closestX = JMap.vertexes[JMap.segments[j].v0].x + (dot * (JMap.vertexes[JMap.segments[j].v1].x - JMap.vertexes[JMap.segments[j].v0].x));
+                var closestY = JMap.vertexes[JMap.segments[j].v0].y + (dot * (JMap.vertexes[JMap.segments[j].v1].y - JMap.vertexes[JMap.segments[j].v0].y));
+                var closestPoint = { x: closestX, y: closestY };
+                console.log("distancetov0: " + distancetov0 + "\ndistancetov1: " + distancetov1 + "\nlength: " + length + "\ndot: " + dot + "\nclosestPoint: {" + closestPoint.x + "," + closestPoint.y + "}"); //Before handling this function in room.onGameTick(), toggle this row in command line.
+
+                if (pointDistance(closestPoint, JMap.vertexes[JMap.segments[j].v0]) + pointDistance(closestPoint, JMap.vertexes[JMap.segments[j].v1]) == length) {
+                    distX = closestX - players[i].position.x;
+                    distY = closestY - players[i].position.y;
+                    var distancetosegment = Math.sqrt(Math.pow(distX, 2) + Math.pow(distY, 2));
+
+                    if (0 <= Math.hypot(room.getPlayerDiscProperties(players[i].id).xspeed, room.getPlayerDiscProperties(players[i].id).yspeed)) {
+                        if (JMap.segments[j].cMask != undefined && JMap.segments[j].cMask.includes("red") == true && JMap.segments[j].bCoef != undefined && JMap.segments[j].curve == undefined || (JMap.segments[j].curve != undefined && JMap.segments[j].curve == 0)) { //Red team collision condition for parkour maps
+                            if (distancetosegment <= room.getPlayerDiscProperties(players[i].id).radius + 0.01) { //0.01 is the tolerance
+                                if (room.getPlayerDiscProperties(players[i].id) != null) {
+                                    console.log(new Date().getHours() + ":" + new Date().getMinutes() + ":" + new Date().getSeconds() + "." + new Date().getMilliseconds() + " 💥 " + players[i].name + " has collided with the point {" + closestX + "," + closestY + "} located on a wall on the point {" + room.getPlayerDiscProperties(players[i].id).x + "," + room.getPlayerDiscProperties(players[i].id).y + "}.");
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
 room.onGamePaused = function(byPlayer){
 	
 }
@@ -356,25 +415,8 @@ room.onGameTick = function(){
     endRaceSession();
     currentTime += 1/60;
     // var validCam = null;
-    let players = room.getPlayerList().filter(p => room.getPlayerDiscProperties(p.id) != null && playerList[p.name].isInTheTrack == true);
-    if (currentTime >= 28) {
-	if(Math.floor(currentTime) % 5 == 0) {
-	camId = Math.ceil(Math.random() * players.length);
-	// players.forEach(p => {
-	// 	if(playerList[p.name].id==camId){
-	// 		validCam = true;
-	// 	};
-	//  });
-	}
-	if(camId != null && room.getPlayerDiscProperties(camId) != null && room.getPlayerDiscProperties(camId).x != null) {
-            // room.setDiscProperties(0,{x: room.getPlayerDiscProperties(camId).x+randomDistance});
-            // room.setDiscProperties(0,{y: room.getPlayerDiscProperties(camId).y+randomDistance});
-            room.setDiscProperties(0,{x: room.getPlayerDiscProperties(camId).x});
-            room.setDiscProperties(0,{y: room.getPlayerDiscProperties(camId).y});
-	    room.setDiscProperties(0,{xspeed: 0});
-            room.setDiscProperties(0,{yspeed: 0})
-    	}
-    	};
+    collisionDetectionSegmentPlayer();
+    runCamera();
 }
 
 room.onGameUnpaused = function(byPlayer){
@@ -430,6 +472,8 @@ room.onPlayerChat = function(player,message){
 	    else{
 		room.setCustomStadium(Circuits[id-1]);
 		room.sendAnnouncement(`${_Circuits[id-1].Name} was loaded by ${player.name}`,null,colors.mapLoad,fonts.mapLoad,sounds.mapLoad);
+		currentCircuit = id;
+		JMap = JSON.parse(Circuits[id-1]);
 	    }
 	    
 	    return false;
