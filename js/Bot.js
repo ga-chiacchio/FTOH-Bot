e4822560a46e2b99c55cb8d86d49bdb36c727524
Gabriel Chiacchio <97293408+ga-chiacchio@users.noreply.github.com>
2024-05-22 21:59:35 -0300

    Update Bot.js

--- js/Bot.js
+++ js/Bot.js
@@ -451,11 +451,13 @@ function checkPlayerLaps(){
 // }
 
 function randomNum(min, max) { 
-var n = []; 
-for(var i=0;i<3;i++){ 
-n.push(Math.floor(Math.random() * max) + min); 
-} 
-return n; 
+// var n = []; 
+// for(var i=0;i<3;i++){ 
+// n.push(Math.floor(Math.random() * max) + min); 
+// } 
+// return n;
+    var n = Math.floor(Math.random() * max) + min;
+    return n;
 }
 
 function endRaceSession(){
